2b1b494afebda9d2697ff8efa89e2a2bbe26a2cf
Jaskeerat Singh Bhatia <jaskeerat.rsb@gmail.com>
2017-09-11 06:52:01 -0400

    Changes to cell layout

--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -43,7 +43,7 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 105, height: 105)
+        return CGSize(width: 100, height: 100)
     }
     
     
